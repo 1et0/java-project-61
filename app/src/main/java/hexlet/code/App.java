804813c372f4bba2c6e96356a293b9c52ad9e17f
1et0 <4066934@gmail.com>
2022-11-25 10:13:49 +0300

    creat gitignore

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
-package hexlet.code;
+package hexlet;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
